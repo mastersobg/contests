1d93032030b5f13b680495f37a5dab557a310419
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2012-05-16 00:45:39 +0400

    added template for timus

---
+++ acm.timus.ru/template/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdio>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cstring>
+#include<memory.h>
+#include<cmath>
+#include<cassert>
+#include<sstream>
+#include<cstdlib>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<pii> vpii;
+
+#define x first
+#define y second
+#define mp make_pair
+#define pb push_back
+#define dbg( x ) { cerr << #x << "=" << x << endl; }
+#define dbgv( v ) { cerr << #v << "={";for( int I=0;I<(int)(v).size();++I)cerr << " " << (v)[i];cerr<<" }\n"; }
+#define dbgm( v, n ) { cerr << #v << "={";for( int I=0;I<n;++I)cerr << " " << (v)[i];cerr<<" }\n"; }
+
+const int INF = 1 << 29;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    return 0;
+}
+
