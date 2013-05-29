782742f4f9f1a1eaa125c31e72887e2878d3e4e6
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2013-05-30 00:50:58 +0400

    SRM 579 DIV 2

---
+++ topcoder/src/MarblePositioning.java
@@ -0,0 +1,256 @@
+import java.util.*;
+
+import java.io.*;
+
+import static java.lang.Math.*;
+
+public class MarblePositioning {
+
+    double rec(int idx, int []R, int []r, boolean []used) {
+        if(idx == R.length) {
+            return calc(r);
+        }
+        double ret = 1e+20;
+        for(int i = 0; i < R.length; ++i)
+            if(!used[i]) {
+                used[i] = true;
+                r[idx] = R[i];
+                ret = min(ret, rec(idx + 1, R, r, used));
+                used[i] = false;
+            }
+        return ret;
+    }
+
+    double calc(int []rad) {
+        dbg("rad=", Arrays.toString(rad));
+        double []pos = new double[rad.length];
+        pos[0] = 0;
+        for(int it = 1; it < rad.length; ++it) {
+            pos[it] = can(it, 0.0, pos, rad);
+/*            double l = pos[it - 1];
+            double r = 1e+20;
+            for(int i = 0; i < 10000; ++i) {
+                double mid = (r + l) / 2.0;
+                if(can(it, mid, pos, rad)) {
+                    r = mid;
+                } else {
+                    l = mid;
+                }
+            }
+            pos[it] = r;
+  */      }
+        double ret = pos[rad.length - 1] - pos[0];
+        dbg("pos=", Arrays.toString(pos));
+        dbg("ret=", ret);
+        return ret;
+    }
+
+    double can(int idx, double cur, double []pos, int []r) {
+        double mx = 0.0;
+        for(int i = 0; i < idx; ++i) {
+            double dx = pos[i] + 2.0 * sqrt((double) r[idx] * (double) r[i]);
+//            double dx = sqrt(sqr(r[idx] + r[i]) - sqr(r[idx] - r[i] + 0.0));
+            mx = max(mx, dx);
+/*            double d = dist(pos[i], r[i], cur, r[idx]);
+            if(less(d, sqr(r[i] + r[idx]))) {
+                return false;
+            }
+*/
+        }
+        return mx;
+    }
+
+    boolean equals(double a, double b) {
+        return abs(a - b) <= 1e-20;
+    }
+    boolean less(double a, double b) {
+        return a < b && !equals(a, b);
+    }
+
+    double dist(double x1, double y1, double x2, double y2) {
+        return sqr(x1 - x2) + sqr(y1 - y2);
+    }
+    double sqr(double x) {
+        return x * x;
+    }
+
+    public double totalWidth(int[] r) {
+        return rec(0, r, new int[r.length], new boolean[r.length]);
+    }
+
+    boolean debug = false;
+
+    void dbg(Object ...args) {
+        if(!debug)
+            return ;
+        for(Object o : args)
+            System.err.print(o + " ");
+        System.err.println();
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+        try {
+/*            eq(0,(new MarblePositioning()).totalWidth(new int[] {1, 2}),2.8284271247461903);
+            eq(1,(new MarblePositioning()).totalWidth(new int[] {7,7,7}),28.0);
+            eq(2,(new MarblePositioning()).totalWidth(new int[] {10, 20, 30}),62.92528739883945);
+            eq(3,(new MarblePositioning()).totalWidth(new int[] {100, 100,11,11,25}),200.0);
+  */          eq(4,(new MarblePositioning()).totalWidth(new int[] {1,1,999950884}),63246.0);
+        } catch( Exception exx) {
+            System.err.println(exx);
+            exx.printStackTrace(System.err);
+        }
+    }
+    private static void eq( int n, int a, int b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+    }
+    private static void eq( int n, char a, char b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected '"+b+"', received '"+a+"'.");
+    }
+    private static void eq( int n, long a, long b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected \""+b+"L, received "+a+"L.");
+    }
+    private static void eq( int n, boolean a, boolean b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+    }
+    private static void eq( int n, double a, double b ) {
+        if ( eq(a,b) )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+    }
+    private static void eq( int n, String a, String b ) {
+        if ( a != null && a.equals(b) )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected \""+b+"\", received \""+a+"\".");
+    }
+    private static void eq( int n, int[] a, int[] b ) {
+        if ( a.length != b.length ) {
+            System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+            return;
+        }
+        for ( int i= 0; i < a.length; i++)
+            if ( a[i] != b[i] ) {
+                System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+                print( b );
+                print( a );
+                return;
+            }
+        System.err.println("Case "+n+" passed.");
+    }
+    private static void eq( int n, long[] a, long[] b ) {
+        if ( a.length != b.length ) {
+            System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+            return;
+        }
+        for ( int i= 0; i < a.length; i++ )
+            if ( a[i] != b[i] ) {
+                System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+                print( b );
+                print( a );
+                return;
+            }
+        System.err.println("Case "+n+" passed.");
+    }
+    private static void eq( int n, double[] a, double[] b ) {
+        if ( a.length != b.length ) {
+            System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+            return;
+        }
+        for ( int i= 0; i < a.length; i++ )
+            if ( !eq(a[i], b[i]) ) {
+                System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+                print( b );
+                print( a );
+                return;
+            }
+        System.err.println("Case "+n+" passed.");
+    }
+
+    private static void eq( int n, String[] a, String[] b ) {
+        if ( a.length != b.length) {
+            System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+            return;
+        }
+        for ( int i= 0; i < a.length; i++ )
+            if( !a[i].equals( b[i])) {
+                System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+                print( b );
+                print( a );
+                return;
+            }
+        System.err.println("Case "+n+" passed.");
+    }
+    private static void print( int a ) {
+        System.err.print(a+" ");
+    }
+    private static void print( long a ) {
+        System.err.print(a+"L ");
+    }
+    private static void print( String s ) {
+        System.err.print("\""+s+"\" ");
+    }
+    private static void print( int[] rs ) {
+        if ( rs == null) return;
+        System.err.print('{');
+        for ( int i= 0; i < rs.length; i++ ) {
+            System.err.print(rs[i]);
+            if ( i != rs.length-1 )
+                System.err.print(", ");
+        }
+        System.err.println('}');
+    }
+    private static void print( long[] rs) {
+        if ( rs == null ) return;
+        System.err.print('{');
+        for ( int i= 0; i < rs.length; i++ ) {
+            System.err.print(rs[i]);
+            if ( i != rs.length-1 )
+                System.err.print(", ");
+        }
+        System.err.println('}');
+    }
+    private static void print( double[] rs) {
+        if ( rs == null ) return;
+        System.err.print('{');
+        for ( int i= 0; i < rs.length; i++ ) {
+            System.err.print(rs[i]);
+            if ( i != rs.length-1 )
+                System.err.print(", ");
+        }
+        System.err.println('}');
+    }
+    private static void print( String[] rs ) {
+        if ( rs == null ) return;
+        System.err.print('{');
+        for ( int i= 0; i < rs.length; i++ ) {
+            System.err.print( "\""+rs[i]+"\"" );
+            if( i != rs.length-1)
+                System.err.print(", ");
+        }
+        System.err.println('}');
+    }
+    private static void nl() {
+        System.err.println();
+    }
+
+    private static double eps=1e-9;
+
+    private static boolean eq(double a, double b ) {
+    	return abs(a-b) <= eps;
+    }
+// END CUT HERE
+}
