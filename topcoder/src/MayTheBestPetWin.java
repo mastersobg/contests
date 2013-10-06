09344af377029741c3ce19bcf6b156c0925c6988
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2013-10-06 18:51:36 +0400

    The latest SRM

---
+++ topcoder/src/MayTheBestPetWin.java
@@ -0,0 +1,254 @@
+import java.util.*;
+
+import java.io.*;
+
+import static java.lang.Math.*;
+
+public class MayTheBestPetWin {
+
+
+  int []a, b;
+  boolean [][]d;
+  int n;
+
+  void rec(int idx, int sum) {
+    if (d[idx][sum]) {
+      return ;
+    }
+    d[idx][sum] = true;
+    if (idx == 5 && sum == 10000 * 50 + 1) {
+    	int k = 1;
+    	++k;
+    }
+    if (idx == a.length) {
+        return ;
+      }
+//    rec(idx + 1, sum + a[idx]);
+    int mx = max(a[idx], b[idx]);
+    int mn = min(a[idx], b[idx]);
+    rec(idx + 1, sum + a[idx] + b[idx]);
+    rec(idx + 1, sum);
+//    rec(idx + 1, sum - b[idx]);
+  }
+  public int calc(int[] A, int[] B) {
+    a = A;
+    b = B;
+    n = a.length;
+    d = new boolean[n + 1][10000 * 50 * 2 + 1];
+    rec(0, 10000 * 50);
+    int ret = 1 << 30;
+    for (int i = 0; i < d[n].length; ++i) {
+      if (d[n][i]) {
+        int actual = abs(i - 10000 * 50);
+        ret = min(ret, actual);
+      }
+    }
+    return ret;
+  }
+
+  
+  // BEGIN CUT HERE
+  static boolean DEBUG = false;
+
+  void dbg(Object ... objs) {
+    if (!DEBUG) {
+      return ;
+    }
+    for (Object o : objs) {
+      String printLine;
+      if (o.getClass().isArray()) {
+        printLine = arrayToString(o);
+      } else {
+        printLine = o.toString();
+      }
+      System.err.print(printLine + " ");
+    }
+    System.err.println();
+  }
+
+  String arrayToString(Object o) {
+    if (o instanceof long[]) 
+      return Arrays.toString((long[]) o);
+    if (o instanceof int[])
+      return Arrays.toString((int[]) o);
+    if (o instanceof short[])
+      return Arrays.toString((short[]) o);
+    if (o instanceof char[])
+      return Arrays.toString((char[]) o);
+    if (o instanceof byte[])
+      return Arrays.toString((byte[]) o);
+    if (o instanceof double[])
+      return Arrays.toString((double[]) o);
+    if (o instanceof float[])
+      return Arrays.toString((float[]) o);
+    if (o instanceof boolean[])
+      return Arrays.toString((boolean[]) o);
+    if (o instanceof Object[])
+      return Arrays.deepToString((Object[]) o);
+    throw new IllegalStateException();
+  }
+
+  public static void main(String[] args) {
+    try {
+      DEBUG = true;
+//                  eq(0,(new MayTheBestPetWin()).calc(new int[] {3,4,4,7}, new int[] {3,4,4,7}),2);
+            eq(1,(new MayTheBestPetWin()).calc(new int[] {1,3,5,4,5}, new int[] {2,5,6,8,7}),5);
+//            eq(2,(new MayTheBestPetWin()).calc(new int[] {2907,949,1674,6092,8608,5186,2630,970,1050,2415,1923,2697,5571,6941,8065,4710,716,756,5185,1341,993,5092,248,1895,4223,1783,3844,3531,2431,1755,2837,4015}, new int[] {7296,6954,4407,9724,8645,8065,9323,8433,1352,9618,6487,7309,9297,8999,9960,5653,4721,7623,6017,7320,3513,6642,6359,3145,7233,5077,6457,3605,2911,4679,5381,6574}),52873);
+    } catch( Exception exx) {
+      System.err.println(exx);
+      exx.printStackTrace(System.err);
+    }
+  }
+  private static void eq( int n, int a, int b ) {
+    if ( a==b )
+      System.err.println("Case "+n+" passed.");
+    else
+      System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+  }
+  private static void eq( int n, char a, char b ) {
+    if ( a==b )
+      System.err.println("Case "+n+" passed.");
+    else
+      System.err.println("Case "+n+" failed: expected '"+b+"', received '"+a+"'.");
+  }
+  private static void eq( int n, long a, long b ) {
+    if ( a==b )
+      System.err.println("Case "+n+" passed.");
+    else
+      System.err.println("Case "+n+" failed: expected \""+b+"L, received "+a+"L.");
+  }
+  private static void eq( int n, boolean a, boolean b ) {
+    if ( a==b )
+      System.err.println("Case "+n+" passed.");
+    else
+      System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+  }
+  private static void eq( int n, double a, double b ) {
+    if ( eq(a,b) )
+      System.err.println("Case "+n+" passed.");
+    else
+      System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+  }
+  private static void eq( int n, String a, String b ) {
+    if ( a != null && a.equals(b) )
+      System.err.println("Case "+n+" passed.");
+    else
+      System.err.println("Case "+n+" failed: expected \""+b+"\", received \""+a+"\".");
+  }
+  private static void eq( int n, int[] a, int[] b ) {
+    if ( a.length != b.length ) {
+      System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+      return;
+    }
+    for ( int i= 0; i < a.length; i++)
+      if ( a[i] != b[i] ) {
+        System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+        print( b );
+        print( a );
+        return;
+      }
+    System.err.println("Case "+n+" passed.");
+  }
+  private static void eq( int n, long[] a, long[] b ) {
+    if ( a.length != b.length ) {
+      System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+      return;
+    }
+    for ( int i= 0; i < a.length; i++ )
+      if ( a[i] != b[i] ) {
+        System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+        print( b );
+        print( a );
+        return;
+      }
+    System.err.println("Case "+n+" passed.");
+  }
+  private static void eq( int n, double[] a, double[] b ) {
+    if ( a.length != b.length ) {
+      System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+      return;
+    }
+    for ( int i= 0; i < a.length; i++ )
+      if ( !eq(a[i], b[i]) ) {
+        System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+        print( b );
+        print( a );
+        return;
+      }
+    System.err.println("Case "+n+" passed.");
+  }
+
+  private static void eq( int n, String[] a, String[] b ) {
+    if ( a.length != b.length) {
+      System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+      return;
+    }
+    for ( int i= 0; i < a.length; i++ )
+      if( !a[i].equals( b[i])) {
+        System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+        print( b );
+        print( a );
+        return;
+      }
+    System.err.println("Case "+n+" passed.");
+  }
+  private static void print( int a ) {
+    System.err.print(a+" ");
+  }
+  private static void print( long a ) {
+    System.err.print(a+"L ");
+  }
+  private static void print( String s ) {
+    System.err.print("\""+s+"\" ");
+  }
+  private static void print( int[] rs ) {
+    if ( rs == null) return;
+    System.err.print('{');
+    for ( int i= 0; i < rs.length; i++ ) {
+      System.err.print(rs[i]);
+      if ( i != rs.length-1 )
+        System.err.print(", ");
+    }
+    System.err.println('}');
+  }
+  private static void print( long[] rs) {
+    if ( rs == null ) return;
+    System.err.print('{');
+    for ( int i= 0; i < rs.length; i++ ) {
+      System.err.print(rs[i]);
+      if ( i != rs.length-1 )
+        System.err.print(", ");
+    }
+    System.err.println('}');
+  }
+  private static void print( double[] rs) {
+    if ( rs == null ) return;
+    System.err.print('{');
+    for ( int i= 0; i < rs.length; i++ ) {
+      System.err.print(rs[i]);
+      if ( i != rs.length-1 )
+        System.err.print(", ");
+    }
+    System.err.println('}');
+  }
+  private static void print( String[] rs ) {
+    if ( rs == null ) return;
+    System.err.print('{');
+    for ( int i= 0; i < rs.length; i++ ) {
+      System.err.print( "\""+rs[i]+"\"" );
+      if( i != rs.length-1)
+        System.err.print(", ");
+    }
+    System.err.println('}');
+  }
+  private static void nl() {
+    System.err.println();
+  }
+
+  private static double eps=1e-9;
+
+  private static boolean eq(double a, double b ) {
+    return abs(a-b) <= eps;
+  }
+  // END CUT HERE
+}
