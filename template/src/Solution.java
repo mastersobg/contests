4c5438ee1dd10b2b48eda5d937a3a05815f27c38
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2013-10-19 13:43:10 +0400

    Modified template

--- template/src/Solution.java
+++ template/src/Solution.java
@@ -5,103 +5,107 @@ import static java.lang.Math.*;
 
 public class Solution {
 
-  static final boolean DEBUG = false;
+    BufferedReader in;
+    StringTokenizer st;
+    PrintWriter out;
 
-	BufferedReader in;
-	StringTokenizer st;
-	PrintWriter out;
-
-	void solve() throws IOException {
-	}
-
-  void dbg(Object ... objs) {
-    if (!DEBUG) {
-      return ;
+    void solve() throws IOException {
     }
-    for (Object o : objs) {
-      String printLine;
-      if (o.getClass().isArray()) {
-        printLine = arrayToString(o);
-      } else {
-        printLine = o.toString();
-      }
-      System.err.print(printLine + " ");
+
+    public void run() throws IOException {
+        Locale.setDefault(Locale.US);
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+        solve();
+        in.close();
+        out.close();
     }
-    System.err.println();
-  }
 
-  String arrayToString(Object o) {
-    if (o instanceof long[]) 
-      return Arrays.toString((long[]) o);
-    if (o instanceof int[])
-      return Arrays.toString((int[]) o);
-    if (o instanceof short[])
-      return Arrays.toString((short[]) o);
-    if (o instanceof char[])
-      return Arrays.toString((char[]) o);
-    if (o instanceof byte[])
-      return Arrays.toString((byte[]) o);
-    if (o instanceof double[])
-      return Arrays.toString((double[]) o);
-    if (o instanceof float[])
-      return Arrays.toString((float[]) o);
-    if (o instanceof boolean[])
-      return Arrays.toString((boolean[]) o);
-    if (o instanceof Object[])
-      return Arrays.deepToString((Object[]) o);
-    throw new IllegalStateException();
-  }
-  
+    String ns() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(in.readLine());
+        return st.nextToken();
+    }
 
-	public void run() throws IOException {
-		Locale.setDefault(Locale.US);
-		in = new BufferedReader(new InputStreamReader(System.in));
-		out = new PrintWriter(System.out);
-		solve();
-		in.close();
-		out.close();
-	}
+    int ni() throws IOException {
+        return Integer.valueOf(ns());
+    }
 
-	String ns() throws IOException {
-		while (st == null || !st.hasMoreTokens())
-			st = new StringTokenizer(in.readLine());
-		return st.nextToken();
-	}
+    long nl() throws IOException {
+        return Long.valueOf(ns());
+    }
 
-	int ni() throws IOException {
-		return Integer.valueOf(ns());
-	}
+    double nd() throws IOException {
+        return Double.valueOf(ns());
+    }
 
-	long nl() throws IOException {
-		return Long.valueOf(ns());
-	}
+    public static void main(String[] args) throws IOException {
+        if ("true".equals(System.getProperty("LOCAL_DEBUG"))) {
+            DEBUG = true;
+        }
+        new Solution().run();
+    }
 
-	double nd() throws IOException {
-		return Double.valueOf(ns());
-	}
+    class Timer {
 
-	public static void main(String[] args) throws IOException {
-		new Solution().run();
-	}
+        long time;
 
-  class Timer {
+        void start() {
+            time = System.currentTimeMillis();
+        }
+        long time() {
+            return System.currentTimeMillis() - time;
+        }
+        void print() {
+            print("Time spent = ");
+        } 
+
+        void print(String message) {
+            dbg(message, time());
+        }
+
+    }
+
+    static boolean DEBUG = false;
+
+    void dbg(Object ... objs) {
+        if (!DEBUG) {
+            return ;
+        }
+        for (Object o : objs) {
+            String printLine;
+            if (o.getClass().isArray()) {
+                printLine = arrayToString(o);
+            } else {
+                printLine = o.toString();
+            }
+            System.err.print(printLine + " ");
+        }
+        System.err.println();
+    }
+
+    String arrayToString(Object o) {
+        if (o instanceof long[]) 
+            return Arrays.toString((long[]) o);
+        if (o instanceof int[])
+            return Arrays.toString((int[]) o);
+        if (o instanceof short[])
+            return Arrays.toString((short[]) o);
+        if (o instanceof char[])
+            return Arrays.toString((char[]) o);
+        if (o instanceof byte[])
+            return Arrays.toString((byte[]) o);
+        if (o instanceof double[])
+            return Arrays.toString((double[]) o);
+        if (o instanceof float[])
+            return Arrays.toString((float[]) o);
+        if (o instanceof boolean[])
+            return Arrays.toString((boolean[]) o);
+        if (o instanceof Object[])
+            return Arrays.deepToString((Object[]) o);
+        throw new IllegalStateException();
+    }
+
     
-    long time;
-    
-    void start() {
-      time = System.currentTimeMillis();
-    }
-    long time() {
-      return System.currentTimeMillis() - time;
-    }
-    void print() {
-      print("Time spent = ");
-    } 
-    
-    void print(String message) {
-      dbg(message, time());
-    }
-
-  }
 }
 
