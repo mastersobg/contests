6ba1a95a5219a58426ce1c30e07e9459059ad1e0
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2014-01-19 13:39:14 +0400

    Timus problems

---
+++ acm.timus.ru/1789/src/Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class Solution {
+
+	BufferedReader in;
+	StringTokenizer st;
+	PrintWriter out;
+
+	void solve() throws IOException {
+		int n = ni();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 1; i <= n; ++i)
+			list.add(i);
+		for (int i = n; i >= 1; --i)
+			list.add(i);
+		out.println(list.size());
+		for (int i = 0; i < list.size(); ++i)
+			out.print(list.get(i) + " ");
+	}
+
+	public Solution() throws IOException {
+		Locale.setDefault(Locale.US);
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		solve();
+		in.close();
+		out.close();
+	}
+
+	String ns() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(in.readLine());
+		return st.nextToken();
+	}
+
+	int ni() throws IOException {
+		return Integer.valueOf(ns());
+	}
+
+	long nl() throws IOException {
+		return Long.valueOf(ns());
+	}
+
+	double nd() throws IOException {
+		return Double.valueOf(ns());
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Solution();
+	}
+}
