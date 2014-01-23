e3fac19b8290097f7a105420d6911656c2cebc97
Ivan Gorbachev <ivan.gorbachev@odnoklassniki.ru>
2014-01-23 11:12:20 +0400

    WA 11

---
+++ acm.sgu.ru/275/src/Solution.java
@@ -0,0 +1,130 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class Solution {
+
+	BufferedReader in;
+	StringTokenizer st;
+	PrintWriter out;
+
+	void solve() throws IOException {
+		int n = ni();
+		long[] v = new long[n];
+		long[][] dp = new long[2][n];
+		for (int i = 0; i < n; ++i) {
+			v[i] = nl();
+			dp[1][i] = v[i];
+		}
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < 2; ++j) {
+				long mx = 0;
+				int rj = j ^ 1;
+				for (int k = 0; k < i; ++k) {
+					mx = max(dp[rj][k] ^ v[i], mx);
+				}
+				dp[j][i] = max(dp[j][i], mx);
+			}
+		}
+		long mx = 0;
+		for (int i = 0; i < 2; ++i)
+			for (int j = 0; j < n; ++j)
+				mx = max(dp[i][j], mx);
+		out.println(mx);
+	}
+
+	public void run() throws IOException {
+		Locale.setDefault(Locale.US);
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		solve();
+		in.close();
+		out.close();
+	}
+
+	String ns() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(in.readLine());
+		return st.nextToken();
+	}
+
+	int ni() throws IOException {
+		return Integer.valueOf(ns());
+	}
+
+	long nl() throws IOException {
+		return Long.valueOf(ns());
+	}
+
+	double nd() throws IOException {
+		return Double.valueOf(ns());
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Solution().run();
+	}
+
+	class Timer {
+
+		long time;
+
+		void start() {
+			time = System.currentTimeMillis();
+		}
+
+		long time() {
+			return System.currentTimeMillis() - time;
+		}
+
+		void print() {
+			print("Time spent = ");
+		}
+
+		void print(String message) {
+			dbg(message, time());
+		}
+
+	}
+
+	static boolean DEBUG = false;
+
+	void dbg(Object... objs) {
+		if (!DEBUG) {
+			return;
+		}
+		for (Object o : objs) {
+			String printLine;
+			if (o.getClass().isArray()) {
+				printLine = arrayToString(o);
+			} else {
+				printLine = o.toString();
+			}
+			System.err.print(printLine + " ");
+		}
+		System.err.println();
+	}
+
+	String arrayToString(Object o) {
+		if (o instanceof long[])
+			return Arrays.toString((long[]) o);
+		if (o instanceof int[])
+			return Arrays.toString((int[]) o);
+		if (o instanceof short[])
+			return Arrays.toString((short[]) o);
+		if (o instanceof char[])
+			return Arrays.toString((char[]) o);
+		if (o instanceof byte[])
+			return Arrays.toString((byte[]) o);
+		if (o instanceof double[])
+			return Arrays.toString((double[]) o);
+		if (o instanceof float[])
+			return Arrays.toString((float[]) o);
+		if (o instanceof boolean[])
+			return Arrays.toString((boolean[]) o);
+		if (o instanceof Object[])
+			return Arrays.deepToString((Object[]) o);
+		throw new IllegalStateException();
+	}
+
+}
