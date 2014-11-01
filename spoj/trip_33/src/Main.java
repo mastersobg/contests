db15ba6c3acb918dd762b9c57d6b5d3235d68f1c
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2014-11-01 21:06:20 +0300

    Trip AC

--- spoj/trip_33/src/Main.java
+++ spoj/trip_33/src/Main.java
@@ -12,90 +12,47 @@ public class Main {
 	void solve() throws IOException {
         boolean first = true;
         for (int t = ni(); t > 0; --t) {
-            if (first)
+            if (!first)
                 out.println();
             first = false;
             char []s1 = ns().toCharArray();
             char []s2 = ns().toCharArray();
             int [][]dp = new int[s1.length + 1][s2.length + 1];
-            List<Integer> [][]prev = new List[s1.length + 1][s2.length + 1];
+            Set<String> [][]prev = new Set[s1.length + 1][s2.length + 1];
             for (int i = 0; i < dp.length; ++i) {
                 Arrays.fill(dp[i], Integer.MIN_VALUE);
                 for (int j = 0; j < dp[i].length; ++j) {
-                    prev[i][j] = new ArrayList<Integer>();
+                    prev[i][j] = new TreeSet<String>();
                 }
             }
+            prev[0][0].add("");
             dp[0][0] = 0;
             for (int i = 0; i <= s1.length; ++i) {
                 for (int j = 0; j <= s2.length; ++j) {
-                    relax(dp, prev, i, j, i + 1, j, dp[i][j]);
-                    relax(dp, prev, i, j, i, j + 1, dp[i][j]);
+                    relax(dp, prev, i, j, i + 1, j, dp[i][j], s1);
+                    relax(dp, prev, i, j, i, j + 1, dp[i][j], s1);
                     if (i < s1.length && j < s2.length && s1[i] == s2[j]) {
-                        relax(dp, prev, i, j, i + 1, j + 1, dp[i][j] + 1);
+                        relax(dp, prev, i, j, i + 1, j + 1, dp[i][j] + 1, s1);
                     }
                 }
             }
-            TreeSet<String> ans = new TreeSet<String>();
-            restore(s1, prev, s1.length, s2.length, "", ans);
-            for (String s : ans)
+            for (String s : prev[s1.length][s2.length])
                 out.println(s);
         }
 	}
-
-
-    static class Entry {
-
-        int i, j;
-        String s;
-
-        Entry(int a, int b, String c) {
-            i = a;
-            j = b;
-            s = c;
-        }
-        @Override
-        public boolean equals(Object o) {
-            Entry e = (Entry) o;
-            return i == e.i && j == e.j && s.equals(e.s);
-        }
-
-        @Override
-        public int hashCode() {
-            return i * 31 * 31 + j * 31 + s.hashCode();
-        }
-
-    }
-
-    HashSet<Entry> cache = new HashSet<Entry>();
-
-    void restore(char []s, List<Integer> [][]pred, int i, int j, String word, Set<String> words) {
-        if (cache.contains(new Entry(i, j, word))) {
-            return;
-        }
-
-        if (i == 0 && j == 0) {
-            words.add(word);
-            return ;
-        }
-        for (int idx = 0; idx < pred[i][j].size(); idx += 2) {
-            int ni = pred[i][j].get(idx);
-            int nj = pred[i][j].get(idx + 1);
-            if (ni != i && nj != j) {
-                restore(s, pred, ni, nj, s[ni] + word, words);
-            } else {
-                restore(s, pred, ni, nj, word, words);
-            }
-        }
-    }
-
-    void relax(int [][]dp, List<Integer> [][]prev, int i, int j, int ni, int nj, int value) {
+    void relax(int [][]dp, Set<String> [][]prev, int i, int j, int ni, int nj, int value, char []s1) {
         if (ni < dp.length && nj < dp[ni].length) {
             if (dp[ni][nj] <= value) {
                 if (dp[ni][nj] < value)
                     prev[ni][nj].clear();    
                 dp[ni][nj] = value;
-                prev[ni][nj].add(i);
-                prev[ni][nj].add(j);
+                for (String a : prev[i][j]) {
+                    String s = a;
+                    if (ni != i && nj != j) {
+                        s = s + s1[i];
+                    }
+                    prev[ni][nj].add(s);
+                } 
             }
         }
     }	
